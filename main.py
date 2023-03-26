d0934c2918ff9a33fba56341abb703719accdfbc
mustafa <mustafa@deta.sh>
2023-03-26 01:43:01 +0100

    add redicrect

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os, random, string
 from starlette.applications import Starlette
-from starlette.responses import HTMLResponse, JSONResponse
+from starlette.responses import HTMLResponse, JSONResponse, RedirectResponse
 from starlette.routing import Route
 from datetime import datetime
 from deta import Base
 
 db = Base("links")
+
 
 async def home(request):
     return HTMLResponse("Hello, world!")
@@ -19,18 +20,26 @@ async def add(request):
         return JSONResponse({"message": "No url provided"}, 400)
     if not payload["url"].startswith("http"):
         return JSONResponse({"message": "Invalid url"}, 400)
-    
+    if "slug" in payload and payload["slug"] == "api":
+        return JSONResponse({"message": "Invalid slug"}, 400)
+
     # TODO: handle when random slug already exists
     try:
-        item = db.insert({
-            "url": payload["url"],
-            "hits": 0,
-            "created_at": datetime.now().isoformat(),
-            "updated_at": datetime.now().isoformat(),
-            "key": payload.get("slug", ''.join(random.choices(string.ascii_lowercase + string.digits, k=6)))
-            })
+        item = db.insert(
+            {
+                "url": payload["url"],
+                "hits": 0,
+                "created_at": datetime.now().isoformat(),
+                "updated_at": datetime.now().isoformat(),
+                "key": payload.get(
+                    "slug",
+                    "".join(
+                        random.choices(string.ascii_lowercase + string.digits, k=6)
+                    ),
+                ),
+            }
+        )
     except Exception as e:
-        
         if e.code == 409:
             return JSONResponse({"message": "Slug already exists"}, 400)
         return JSONResponse({"message": "Something went wrong"}, 500)
@@ -38,8 +47,20 @@ async def add(request):
     return JSONResponse(item)
 
 
+async def redirect(request):
+    slug = request.path_params["slug"]
+    item = db.get(slug)
+    if not item:
+        return HTMLResponse("lonk not found, friendo", 404)
+    db.update({"hits": db.util.increment()}, slug)
+    return RedirectResponse(item["url"])
+
+
 app = Starlette(
     debug=os.getenv("DETA_RUNTIME", True),
-    routes=[Route("/", home, methods=["GET"]), Route("/add", add, methods=["POST"])],
+    routes=[
+        Route("/", home, methods=["GET"]),
+        Route("/api/add", add, methods=["POST"]),
+        Route("/{slug}", redirect, methods=["GET"]),
+    ],
 )
-
